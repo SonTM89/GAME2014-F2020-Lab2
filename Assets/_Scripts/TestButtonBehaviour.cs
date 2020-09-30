c89ed38c64412b376568df649547b69483150a17
Minh Son Tran <tranminhson1989@gmail.com>
2020-09-29 20:12:07 -0400

    initial commit

---
+++ Assets/_Scripts/TestButtonBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    //public TMP_Text SceneLabel;
+    public TMP_Text LiveLabel;
+    public TMP_Text ScoreLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("Test Button Pressed");
+        //SceneLabel.text = "Test";
+        LiveLabel.rectTransform.anchoredPosition = new Vector2(370.0f, -83.0f);
+        ScoreLabel.rectTransform.anchoredPosition = new Vector2(-353.0f, -83.0f);
+    }
+}
